5bf570b60829fbec453bd4f3bf8f813bdfb1fec5
leejne <leejne@qq.com>
2025-03-31 20:47:56 +0800

    4

--- src/workers/websocket_worker.ts
+++ src/workers/websocket_worker.ts
@@ -1,9 +1,11 @@
 /// <reference lib="webworker" />
 
+import { WebSocketClientBase } from "@/core/net/websocket_client";
 import { type IWebSocketCmd, WebSocketCmdConnect } from "./websocket_cmd";
+import { ExponentialRetryStrategy } from "@/core/retry_strategy";
 
 const ports: MessagePort[] = [];
-let websocket: WebSocket | null
+let websocket: WebSocketClient | undefined
 
 const scope = self as unknown as SharedWorkerGlobalScope
 if (!scope) {
@@ -14,11 +16,6 @@ scope.onconnect = (e: MessageEvent) => {
     const port = e.ports[0]
     ports.push(port)
 
-    port.postMessage({
-        cmd: "token",
-        data: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.KMUFsIDTnFmyG3nMiGM6H9FNFUROf3wh7SmqJp-QV30',
-    })
-
     port.onmessage = (e: MessageEvent<IWebSocketCmd>) => {
         console.log(' 收到消息 ', e.data)
         if (e.data.cmd === WebSocketCmdConnect) {
@@ -27,8 +24,117 @@ scope.onconnect = (e: MessageEvent) => {
                 return
             }
 
-            websocket = new WebSocket(e.data.data)
+            websocket = new WebSocketClient(e.data.data)
         }
         ports.forEach(p => p.postMessage("i've received your message"))
     }
 } 
+
+ class WebSocketClient extends WebSocketClientBase {
+    // readonly msgProtocol: MessageProtocol
+ 
+    constructor(url: string) {
+        super(url, ['niu-v1'], 'arraybuffer', new ExponentialRetryStrategy(1000, 5))
+        // this.msgProtocol = msgProtocol
+    }
+
+    onData(data: string | ArrayBuffer): void {
+        if (typeof data == 'string') {
+            console.log('text message: ', data)
+        } else if (data instanceof ArrayBuffer) {
+            // const [msgType, reqId, payload] = this.msgProtocol.decode(new Uint8Array(data))
+            // console.log('binary message: ', msgType, reqId, payload)
+        }
+    }
+
+    onHeartbeatTick(): void {
+        //this.sendMsg(MsgType.ping, new Uint8Array(0));
+    }
+
+    onConnected(): void {
+        console.log('connected')
+    }
+    onWillReconnect(durationMs: number): void {
+        console.log(`reconnect after ${durationMs}ms`)
+    }
+
+    // sendMsg(msgType: MsgType, payload: Uint8Array): RequestId {
+    //     // const id = RequestId.next();
+    //     // const data = this.msgProtocol.encode(msgType, id, payload);
+    //     // this.send(data);
+    //     // return id;
+    // }
+}
+
+
+export enum MsgType {
+    ready = 0,
+    ping = 1,
+    pong = 2,
+
+    bizUserReq = 50,
+    bizUserRes = 51,
+}
+
+export class RequestId {
+    readonly timestamp: number
+    readonly seq: number
+
+    constructor(timestamp: number, seq: number) {
+        this.timestamp = timestamp
+        this.seq = seq
+    }
+
+    static origin = Date.UTC(2024, 1) / 1000
+    static _lastTimestap = 0;
+    static _seq = 0;
+    static next(): RequestId {
+        const timestamp = Date.now() / 1000 -
+            RequestId.origin;
+        if (timestamp == RequestId._lastTimestap) {
+            // 同一秒内
+            RequestId._seq++;
+        } else {
+            RequestId._seq = 0;
+            RequestId._lastTimestap = timestamp;
+        }
+        return new RequestId(timestamp, RequestId._seq);
+    }
+
+    static fromArray(arr: Uint8Array): RequestId {
+        return new RequestId((arr[0] << 24) | (arr[1] << 16) | (arr[2] << 8) | arr[3], arr[4]);
+    }
+
+    toArray(): Uint8Array {
+        const list = new Uint8Array(5)
+        list[0] = (this.timestamp & 0xff000000) >> 24;
+        list[1] = (this.timestamp & 0x00ff0000) >> 16;
+        list[2] = (this.timestamp & 0x0000ff00) >> 8;
+        list[3] = this.timestamp & 0x000000ff;
+        list[4] = this.seq;
+        return list;
+    }
+
+    toString(): string {
+        return `${this.timestamp}:${this.seq}`;
+    }
+}
+
+export class MessageProtocol {
+    encode(msgType: MsgType, reqId: RequestId, payload: Uint8Array): Uint8Array {
+        const list = new Uint8Array(6 + payload.length);
+        list[0] = msgType;
+        list.set(reqId.toArray(), 1)
+        if (payload.length > 0) list.set(payload, 6);
+        return list;
+    }
+
+    decode(data: Uint8Array): [MsgType, RequestId, Uint8Array] {
+        console.assert(data.length >= 6, "must at least have msgtype and request id.");
+        return [
+            data[0],
+            RequestId.fromArray(data.subarray(1, 6)),
+            data.length > 6 ? data.subarray(6) : new Uint8Array(0)
+        ]
+    }
+}

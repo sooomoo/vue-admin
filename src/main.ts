4bad8f4cce174e41954e8874c34d22dabd21093e
mur <mur@qq.com>
2025-04-10 21:31:12 +0800

    1

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,15 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import VueCookies from 'vue-cookies'
 import log from 'loglevel'
 
 import App from './App.vue'
 import router from './router'
 
-log.setLevel(import.meta.env.PROD ?'error':'debug')
+log.setLevel(import.meta.env.PROD ? 'error' : 'debug')
 
 const app = createApp(App)
 
-app.use(VueCookies)
 app.use(createPinia())
 app.use(router)
 
